8d6d163c77c3d3cc882725ca86a0560b81225096
rezwankader <rezwanul.kader06@gmail.com>
2021-07-28 12:09:59 +0600

    'forms_and_user_accounts'

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView  # new
+from django.urls import reverse_lazy  # new
 # Create your views here.
 
 from .models import Post
@@ -8,8 +10,28 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+    # context_object_name = 'all_posts_list'
 
 
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+    # context_object_name = 'indiv_post'
+
+
+class BlogCreateView(CreateView):  # new
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):  # new
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',  # new
+    'accounts.apps.AccountsConfig',  # new
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +125,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'  # new
